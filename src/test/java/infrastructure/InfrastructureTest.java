6d9df2ffcc333a5d142fde4bcc930ab91577a350
leweke <male233@001NTB1066.ad.aconso.com>
2019-11-05 16:25:41 +0100

    speaking names...

--- src/test/java/infrastructure/InfrastructureTest.java
+++ src/test/java/infrastructure/InfrastructureTest.java
@@ -5,46 +5,47 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import infrastructure.entities.Book;
 
+@DisplayName("Tests providing the basic functionality of the infrastructure")
 public class InfrastructureTest extends JPAHibernateTest {
 
     @Test
-    public void testGetObjectById_success() {
+    @DisplayName("Query single object by ID")
+    public void getObjectByID() {
         Book book = em.find(Book.class, 1);
         assertNotNull(book);
     }
 
     @Test
-    public void testGetAll_success() {
+    @DisplayName("Query all entries from one table")
+    public void getAllFromSingleTable() {
         List<Book> books = em.createNamedQuery("Book.getAll", Book.class).getResultList();
         assertEquals(1, books.size());
     }
 
     @Test
+    @DisplayName("Create a new object and persist it")
     public void testPersist_success() {
         em.getTransaction().begin();
         em.persist(new Book(10, "Unit Test Hibernate/JPA with in memory H2 Database"));
         em.getTransaction().commit();
-
         List<Book> books = em.createNamedQuery("Book.getAll", Book.class).getResultList();
-
         assertNotNull(books);
         assertEquals(2, books.size());
     }
 
     @Test
+    @DisplayName("Delete one entry from the database")
     public void testDelete_success() {
         Book book = em.find(Book.class, 1);
-
         em.getTransaction().begin();
         em.remove(book);
         em.getTransaction().commit();
-
         List<Book> books = em.createNamedQuery("Book.getAll", Book.class).getResultList();
-
         assertEquals(0, books.size());
     }
 
