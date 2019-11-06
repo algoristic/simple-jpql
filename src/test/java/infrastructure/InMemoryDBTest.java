77b7f07e198751185324ab217ccb87d88060cb93
leweke <male233@001NTB1066.ad.aconso.com>
2019-11-06 14:31:15 +0100

    typo

--- src/test/java/infrastructure/InMemoryDBTest.java
+++ src/test/java/infrastructure/InMemoryDBTest.java
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.Test;
 
 import infrastructure.entities.Book;
 
-@DisplayName("Tests providing the basic functionality of the infrastructure")
-public class InfrastructureTest extends JPAHibernateTest {
+@DisplayName("Tests providing the basic functionality of the In-Memory DB")
+public class InMemoryDBTest extends JPAHibernateTest {
 
     @Test
     @DisplayName("Query single object by ID")
@@ -29,7 +29,7 @@ public class InfrastructureTest extends JPAHibernateTest {
 
     @Test
     @DisplayName("Create a new object and persist it")
-    public void testPersist_success() {
+    public void persistNewEntry() {
         em.getTransaction().begin();
         em.persist(new Book(10, "Unit Test Hibernate/JPA with in memory H2 Database"));
         em.getTransaction().commit();
@@ -40,7 +40,7 @@ public class InfrastructureTest extends JPAHibernateTest {
 
     @Test
     @DisplayName("Delete one entry from the database")
-    public void testDelete_success() {
+    public void deleteEntry() {
         Book book = em.find(Book.class, 1);
         em.getTransaction().begin();
         em.remove(book);
