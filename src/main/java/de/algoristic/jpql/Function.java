fd4498ede1bcc98de3455e385dd677c5f109890d
leweke <male233@001NTB1066.ad.aconso.com>
2019-12-09 15:49:57 +0100

    implement aggregate functions

--- src/main/java/de/algoristic/jpql/Function.java
+++ src/main/java/de/algoristic/jpql/Function.java
@@ -3,7 +3,7 @@ package de.algoristic.jpql;
 import de.algoristic.jpql.render.FunctionRenderer;
 import de.algoristic.jpql.render.Renderable;
 import de.algoristic.jpql.render.Renderer;
-import de.algoristic.jpql.sql.CommonFunctions;
+import de.algoristic.jpql.sql.AggregateFunctions;
 import de.algoristic.jpql.sql.SQLFunction;
 import de.algoristic.jpql.sql.SharedQueryInformation;
 
@@ -16,6 +16,10 @@ public class Function extends Property implements Renderable {
         super(property.getName());
         this.affectedProperty = property;
         this.function = function;
+    }
+
+    public Function(Table table, SQLFunction function) {
+        this(table.all(), function);
     }
 
     public Property getAffectedProperty() {
@@ -41,8 +45,44 @@ public class Function extends Property implements Renderable {
         affectedProperty.preProcess(queryInfo);
     }
 
+    public static Property min(Property property) {
+        return new Function(property, AggregateFunctions.MIN);
+    }
+
+    public static Property min(Table table) {
+        return new Function(table, AggregateFunctions.MIN);
+    }
+
+    public static Property max(Property property) {
+        return new Function(property, AggregateFunctions.MAX);
+    }
+
+    public static Property max(Table table) {
+        return new Function(table, AggregateFunctions.MAX);
+    }
+
+    public static Property avg(Property property) {
+        return new Function(property, AggregateFunctions.AVG);
+    }
+
+    public static Property avg(Table table) {
+        return new Function(table, AggregateFunctions.AVG);
+    }
+
+    public static Property sum(Property property) {
+        return new Function(property, AggregateFunctions.SUM);
+    }
+
+    public static Property sum(Table table) {
+        return new Function(table, AggregateFunctions.SUM);
+    }
+
     public static Property count(Property property) {
-        return new Function(property, CommonFunctions.COUNT);
+        return new Function(property, AggregateFunctions.COUNT);
+    }
+
+    public static Property count(Table table) {
+        return new Function(table, AggregateFunctions.COUNT);
     }
 
 }
